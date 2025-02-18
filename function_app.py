a5f873119139c0e77321288ae50a845039e7455f
bert-IA <blouviot@gmail.com>
2025-02-18 19:36:55 +0100

    mofification de function_app

--- function_app.py
+++ function_app.py
@@ -22,6 +22,7 @@ logging.basicConfig(level=logging.INFO)
 CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 EVENT_GRID_TOPIC_ENDPOINT = os.getenv("EVENT_GRID_TOPIC_ENDPOINT")
 EVENT_GRID_TOPIC_KEY = os.getenv("EVENT_GRID_TOPIC_KEY")
+CONTAINER_NAME = "input"
 
 if not CONNECTION_STRING:
     logging.error("Azure Storage connection string is not set.")
@@ -33,7 +34,7 @@ if not EVENT_GRID_TOPIC_ENDPOINT or not EVENT_GRID_TOPIC_KEY:
 else:
     logging.info(f"Event Grid topic endpoint: {EVENT_GRID_TOPIC_ENDPOINT}")
 
-CONTAINER_NAME = "input"
+
 
 # Définir l'application Azure Functions
 app = func.FunctionApp()
